6df477b4fcf418102827900580e467d58dd7ff51
Andris <andriss4@inbox.lv>
2022-12-18 15:58:21 +0200

    fixed code copy

--- src/components/shoppingList/ShoppingListMenu.tsx
+++ src/components/shoppingList/ShoppingListMenu.tsx
@@ -26,7 +26,7 @@ export default function ShoppingListMenu({ handleCloseMenu }: Props) {
   }
 
   function handleCopy() {
-    navigator.clipboard.writeText(shopListContext!.id.toString());
+    navigator.clipboard.writeText(shopListContext!.accessKey.toString());
     setCodeCopied(true);
   }
 
