58c9b0325bd41fcf5807064278ec3783ddb9e831
Andris <andriss4@inbox.lv>
2023-01-05 12:00:18 +0200

    changed css order

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./assets/styles/index.css";
 import "./assets/styles/normalize.css";
+import "./assets/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // Developing mode that helps to catch bugs by doubling render() but some things don't work as intended in this mode
