5f255b1945f72700e690876007536d5a43745c87
Andris <andriss4@inbox.lv>
2023-03-05 08:35:53 +0200

    more improvements

--- src/database/products/uploadProductImage.ts
+++ src/database/products/uploadProductImage.ts
@@ -1,14 +1,20 @@
 import { supabase } from "../initialize";
+import Compressor from "compressorjs";
 
 export async function dbUploadProductImage(productImageFile: File) {
-  const { data, error } = await supabase.storage
-    .from("pictures")
-    .upload(productImageFile.name, productImageFile, {
-      cacheControl: "3600",
-      upsert: false,
-    });
+  new Compressor(productImageFile, {
+    quality: 0.6,
+    success: async (compressedResult) => {
+      const { data, error } = await supabase.storage
+        .from("pictures")
+        .upload(compressedResult.name, compressedResult, {
+          cacheControl: "3600",
+          upsert: false,
+        });
 
-  if (error) {
-    console.error(error);
-  }
+      if (error) {
+        console.error(error);
+      }
+    },
+  });
 }
